2670fb0cdda030db45d8ebd8c5cc1b872b1015c9
Vexikoff <vexikoff@gmail.com>
2026-05-09 19:32:36 +0300

    Update frmscursor.js

--- frmscursor.js
+++ frmscursor.js
@@ -3,17 +3,17 @@ async function loadComponentWithAssets(containerId, htmlUrl) {
   const html = await response.text();
   document.getElementById(containerId).innerHTML = html;
 
-  if (!document.querySelector('link[href="stylefrms.css"]')) {
+  if (!document.querySelector('link[href="https://raw.githubusercontent.com/vexikoff/firemos/refs/heads/main/stylefrms.css"]')) {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'stylefrms.css';
+    link.href = 'https://raw.githubusercontent.com/vexikoff/firemos/refs/heads/main/stylefrms.css';
     document.head.appendChild(link);
   }
 
   const script = document.createElement('script');
-  script.src = 'scriptfrms.js';
+  script.src = 'https://raw.githubusercontent.com/vexikoff/firemos/refs/heads/main/scriptfrms.js';
   script.defer = true;
   document.body.appendChild(script);
 }
 
-loadComponentWithAssets('frms', 'indexfrms.html');
+loadComponentWithAssets('frms', 'https://raw.githubusercontent.com/vexikoff/firemos/refs/heads/main/indexfrms.html');
